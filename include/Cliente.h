aff246f53c0802bd2385bdbb83092186f86f7841
Luiz Guilherme C. da Silva <luiz.developer.acc@gmail.com>
2026-02-19 18:41:06 -0300

    Updated Cliente class constructor to include additional parameters: CPF, date of birth, and profile picture path or base64 string.

--- include/Cliente.h
+++ include/Cliente.h
@@ -10,8 +10,14 @@ class Cliente : public Usuario {
 public:
     /**
      * @brief Construtor da classe Cliente.
+     * @param nome Nome do cliente.
+     * @param email Email do cliente.
+     * @param cpf CPF do cliente.
+     * @param dataNascimento Data de nascimento no formato YYYY-MM-DD.
+     * @param fotoPerfil Caminho ou base64 da foto de perfil.
      */
-    Cliente(const QString& nome, const QString& email);
+    Cliente(const QString& nome, const QString& email, const QString& cpf,
+            const QString& dataNascimento, const QString& fotoPerfil);
 
     /**
      * @brief Retorna o tipo do usuário.
