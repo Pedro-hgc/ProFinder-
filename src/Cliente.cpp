be957ad2ee2576d788399148941b28540e37a642
Luiz Guilherme C. da Silva <luiz.developer.acc@gmail.com>
2026-02-19 18:54:58 -0300

    Updated Cliente class constructor to accept additional parameters: CPF, date of birth, and profile picture, aligning with recent enhancements to user data management.

--- src/Cliente.cpp
+++ src/Cliente.cpp
@@ -1,6 +1,7 @@
 #include "../include/Cliente.h"
 
-Cliente::Cliente(const QString& nome, const QString& email)
-    : Usuario(nome, email) {}
+Cliente::Cliente(const QString& nome, const QString& email, const QString& cpf,
+                 const QString& dataNascimento, const QString& fotoPerfil)
+    : Usuario(nome, email, cpf, dataNascimento, fotoPerfil) {}
 
 QString Cliente::getTipo() const { return "Cliente"; }
