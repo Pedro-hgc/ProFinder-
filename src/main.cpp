9d3f288d31859817046d683c06648f69fcd39ac7
Luiz Guilherme C. da Silva <luiz.developer.acc@gmail.com>
2026-02-19 15:20:06 -0300

    Simplesmente carregamos os dados direto do banco de dados agora.

--- src/main.cpp
+++ src/main.cpp
@@ -5,24 +5,15 @@
 #include "../include/GerenciadorUsuarios.h"
 
 int main(int argc, char *argv[]) {
-    QGuiApplication::setApplicationName("ProFinder version 0.0.1");
-    QGuiApplication::setOrganizationName("ProFinder corp.");
-
     QGuiApplication app(argc, argv);
 
+    // O construtor do GerenciadorUsuarios já chamará carregarDados()
     GerenciadorUsuarios gerenciador;
-    gerenciador.cadastrarFornecedor("Carlos Encanador", "carlos@email.com", "Hidráulica");
-    gerenciador.cadastrarFornecedor("Maria Eletricista", "maria@email.com", "Elétrica");
 
     QQmlApplicationEngine engine;
-
-    // Adiciona a pasta 'ui' como caminho de importação para que o main.qml ache as outras telas
     engine.addImportPath(QDir::currentPath() + "/ui");
-
-    // Expõe o backend
     engine.rootContext()->setContextProperty("gerenciador", &gerenciador);
 
-    // Carrega o container principal
     engine.load(QUrl::fromLocalFile("./ui/main.qml"));
 
     if (engine.rootObjects().isEmpty())
