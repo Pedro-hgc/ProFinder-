5ab326d666c555c9331bffaba2b6d2d837d8f305
Luiz Guilherme C. da Silva <luiz.developer.acc@gmail.com>
2026-02-23 11:21:14 -0300

    Now connecting with the database instance

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include <QGuiApplication>
+#include <QMessageBox>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
 #include <QDir>
+#include <QDebug>
 #include "../include/GerenciadorUsuarios.h"
+#include "../include/DatabaseManager.h"
 
 int main(int argc, char *argv[]) {
     QGuiApplication app(argc, argv);
@@ -18,6 +21,13 @@ int main(int argc, char *argv[]) {
 
     if (engine.rootObjects().isEmpty())
         return -1;
+
+    if (!DatabaseManager::instance().connect()) {
+        qDebug() << "Erro de conexão: Não foi Possível se conectar com o Banco de Dados de ProFinder!";
+        return -1;
+    }
+
+    qDebug() << "Conectado ao banco de dados com sucesso!" ;
 
     return app.exec();
 }
