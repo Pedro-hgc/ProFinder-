411ffca330bd56088648d9cfb64f8118d4d19776
Luiz Guilherme C. da Silva <luiz.developer.acc@gmail.com>
2026-02-23 16:12:11 -0300

    Modified to use QVariantMap for Flexibility

--- src/Fornecedor.cpp
+++ src/Fornecedor.cpp
@@ -1,31 +1,31 @@
 #include "../include/Fornecedor.h"
+#include <QVariant>
 
 Fornecedor::Fornecedor(const QString& nome, const QString& email,
                        const QString& cpf, const QString& dataNascimento, const QString& fotoPerfil,
-                       const QString& cpfCnpj, const QString& certificadoAntecedentes,
+                       const QString& certificadoAntecedentes,
                        const QStringList& fotosServico, const QString& descricaoTrabalho,
-                       const QMap<QString, int>& servicosComAnos)
+                       QVariantMap& servicosComAnos)
     : Usuario(nome, email, cpf, dataNascimento, fotoPerfil), 
-      cpfCnpj(cpfCnpj), certificadoAntecedentes(certificadoAntecedentes),
+      certificadoAntecedentes(certificadoAntecedentes),
       fotosServico(fotosServico), descricaoTrabalho(descricaoTrabalho),
       servicosComAnos(servicosComAnos) {}
 
 QString Fornecedor::getTipo() const { return "Fornecedor"; }
 
-QString Fornecedor::getCpfCnpj() const { return cpfCnpj; }
 QString Fornecedor::getCertificadoAntecedentes() const { return certificadoAntecedentes; }
 QStringList Fornecedor::getFotosServico() const { return fotosServico; }
 QString Fornecedor::getDescricaoTrabalho() const { return descricaoTrabalho; }
-QMap<QString, int> Fornecedor::getServicosComAnos() const { return servicosComAnos; }
+QVariantMap& Fornecedor::getServicosComAnos() const { return servicosComAnos; }
 
-void Fornecedor::setCpfCnpj(const QString& cpfCnpj) { this->cpfCnpj = cpfCnpj; }
 void Fornecedor::setCertificadoAntecedentes(const QString& certificado) { certificadoAntecedentes = certificado; }
 void Fornecedor::setFotosServico(const QStringList& fotos) { fotosServico = fotos; }
 void Fornecedor::setDescricaoTrabalho(const QString& descricao) { descricaoTrabalho = descricao; }
-void Fornecedor::setServicosComAnos(const QMap<QString, int>& servicos) { servicosComAnos = servicos; }
+void Fornecedor::setServicosComAnos(const QVariantMap& servicos) { servicosComAnos = servicos; }
 
 void Fornecedor::adicionarServico(const QString& servico, int anos) {
-    servicosComAnos[servico] = anos;
+    QVariant _variant_age(anos);
+    servicosComAnos.insert(servico, _variant_age);
 }
 
 void Fornecedor::removerServico(const QString& servico) {
