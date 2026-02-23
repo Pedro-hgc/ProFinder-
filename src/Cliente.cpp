6ee794fefff81e6a0e97886bc0ab49e426a66cdd
Luiz Guilherme C. da Silva <luiz.developer.acc@gmail.com>
2026-02-23 19:04:20 -0300

    Client Construct now accepts the ID

--- src/Cliente.cpp
+++ src/Cliente.cpp
@@ -1,7 +1,7 @@
 #include "../include/Cliente.h"
 
 Cliente::Cliente(const QString& nome, const QString& email, const QString& cpf,
-                 const QString& dataNascimento, const QString& fotoPerfil)
-    : Usuario(nome, email, cpf, dataNascimento, fotoPerfil) {}
+                 const QString& dataNascimento, const QString& fotoPerfil, qsizetype id)
+    : Usuario(nome, email, cpf, dataNascimento, fotoPerfil, id) {}
 
-QString Cliente::getTipo() const { return "Cliente"; }
+QString Cliente::getTipo() const { return "CLIENTE"; }
